71a8b1035bf612eb9d3fbbf056f255e121a59d1d
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-02-14 19:52:31 +0500

    Add Filter Category Button with the help of ChatGPT

--- src/script.js
+++ src/script.js
@@ -2,7 +2,30 @@ import "./styles.css";
 import logo from './assets/img/logo.png';
 import search from './assets/img/search.png';
 
-// Loading Images
+// ------ LOAD IMAGES ------ START--- //
 const logoImgDiv = document.getElementById('logo').src = logo
 const searchIcon = document.getElementById('search-icon').src = search
+// ------ LOAD IMAGES ------ END--- //
 
+
+// ------ Filter Buttons Styling ------ START--- //
+// Select all buttons in the group
+const filterButtons = document.querySelectorAll('.category')
+let previousButton = document.querySelector('#filter-slider > button:first-of-type');
+
+// Add event listener to each button
+filterButtons.forEach(function(filterButton) {
+  filterButton.addEventListener('click', function() {
+    // Remove "filter-category" class from the previously selected button
+    previousButton.classList.remove('filter-category');
+    // Add "filter-category" class to the clicked button
+    filterButton.classList.add('filter-category');
+    // Update the previously selected button
+    previousButton = filterButton;
+  })
+})
+
+// Set the first button as the active button on page load
+const firstButton = document.querySelector('#filter-slider > button:first-of-type');
+firstButton.classList.add('filter-category');
+// ------ Filter Buttons Styling ------ END--- //
