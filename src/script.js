268aaca88621b0995510080bfa88f3ae377e5d67
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-02-14 18:06:58 +0500

    Add Header to the mobile version

---
+++ src/script.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import logo from './assets/img/logo.png';
+import search from './assets/img/search.png';
+
+// Loading Images
+const logoImgDiv = document.getElementById('logo').src = logo
+const searchIcon = document.getElementById('search-icon').src = search
+
