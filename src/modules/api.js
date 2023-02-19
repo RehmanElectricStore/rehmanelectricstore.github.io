2f19c6985e02d8982266b821f56d91358b121a29
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-02-19 23:17:41 +0500

    Add product Page

--- src/modules/api.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = `https://cdn.contentful.com/spaces/20wsojjwdirk/environments/master/entries/`;
-
-const endpoint = {
-  electricstore: `${baseUrl}?select=&content_type=electricStoreItems`,
-  stitchingmaterial: `${baseUrl}?select=&content_type=stitchingMaterial`,
-};
-
-export default endpoint;
